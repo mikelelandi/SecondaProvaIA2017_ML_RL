0837416d159438e82c9749f53c9c0c9b277b15ec
mikelelandi <34867783+mikelelandi@users.noreply.github.com>
2018-01-13 18:34:43 +0100

    Update graph_mean.py

--- graph_mean.py
+++ graph_mean.py
@@ -4,9 +4,9 @@
 	Author:					Michele Landi / Raoul Landi
 	Modified by:			Michele landi / Raoul Landi
 	Matr Raoul Landi:		0258005
-	Matr Michele Landi:
+	Matr Michele Landi:     0243311
 	Date created:			09/01/2018
-	Date last modified:		09/01/2018
+	Date last modified:		13/01/2018
 	Python Version:			3.6
 	File Versione:			1.0.0
 
@@ -27,17 +27,35 @@ from graph_test import Graph_Test
 def Nodes_Mean(graph):
     """
     :param graph: deve essere uun oggetto di tipo grafo con Lista di adiacenza
-    :return: matrice di dimensioni NxN con i nodi equidistanti per ogni percorso
+    :return: lista del nodo medio o dei nodi medi a parimerito che risultano essere medio per il maggior numero di coppie di numeri
+             esempio di lista :[[m*,c]] tale che m* è ID del nodo, c è il valore del numero delle coppie di nodi
     """
-    # inizializiamo ma latrice M di dimensioni NxN con il valore None
-    M = [[None for x in range(graph.numNodes())] for y in range(graph.numNodes())]
 
-    # otteniamo la matrice con tutti i percorsi minimi del grafo
+    # inizializiamo una lista ListIdNode composta dagli ID dei nodi del grafo, e un valore di contantore associato pari a 0
+    # tempo teorico di esecuzione O(N)
+    listIdNode = []
+    for Node in graph.getNodes():
+        listIdNode.append([Node.id,0])
+
+
+    # inizializiamo una lista ListTemp composta dagli ID della sequenza dei nodi medi di tutte le coppie di nodi possibili
+    # tempo teorico di esecuzione O(1)
+    listTemp=[]
+
+
+    # otteniamo la matrice con tutti i percorsi minimi tra tutte le possibili coppie di nodi del grafo
+    # tempo di esecuzione teorico O(N^3)
     distances = FloydWarshall_ADJ(graph)
 
-    # cerchiamo i percorsi minimi dove possono avere un modo medio tale dist(n1,m) ​=  dist(m,n2)
-    for i in range(0, graph.numNodes()):
-        for j in range(i, graph.numNodes()):
+
+    # cerchiamo i percorsi minimi in cui è possibile calcolare un nodo medio m* tale che dist(n1,m*) ​=  dist(m*,n2)
+    # dato che la matrice dei percorsi minimi è simmetrica ed il grafo non è orientato, l'elaborazione del minimo
+    # è eseguita solo per la parte superiore alla diagonale della matrice
+    # tempo di esecuzione teorico O(N^3)
+    for i in range(0, graph.numNodes()-1):
+        for j in range(i+1, graph.numNodes()):
+            # il calcolo del nodo medio non viene eseguito se non esiste un arco tra la coppia di nodi in esame
+            # e se nel percorso minimo della coppia di nodi sono presenti un numero di nodi pari
             if i != j and distances[i][j] != float('inf') and distances[i][j] % 2 == 0:
                 for m in graph.dfs(i):
                     if distances[i][m] == distances[m][j] and \
@@ -45,35 +63,50 @@ def Nodes_Mean(graph):
                        distances[m][j] != float('inf') and \
                        distances[i][m] != 0 and \
                        distances[m][j] != 0:
-
-                        M[i][j] = m
+                        #individuato se esiste il nodo medio m aggiorniamo la lista listTemp
+                        listTemp.append(m)
                         break
 
-    # return la matrice di M
-    return M
+
+    # procedimento per aggiornare il valore dei contatori associato a ogni nodo nella lista listIdNode
+    # tempo di esecuzione teorico O(N^3)
+    for q in range (0,len(listTemp)):
+        for k in range (0,len(listIdNode)):
+         if listTemp[q]==listIdNode[k][0]:
+            listIdNode[k][1]=listIdNode[k][1]+1
 
 
-def getNodeMedium(graph,n1,n2):
-    """
-    :param graph: deve essere uun oggetto di tipo grafo con Lista di adiacenza
-    :param n1: Id del nodo di partenza del percorso del grafo [graph]
-    :param n2: Id del nodo di destinazione del percorso del grafo [graph]
-    :return: se esiste un nodo medio del percorso tra n1 e n2 ristituisce il suo Id  altrimenti None
-    """
+    # creazione di uno struttura dati ad arrey medNode composto dall'ID e valore del contatore associato
+    # del nodo o dei nodi che risultano essere i nodi medi per il maggior numero di coppie di nodi
+    # tempo di esecuzione teorico O(N)
+    medNode=[]
+    for w in range (0,len(listIdNode)):
+        if len(medNode)==0:
+           medNode = [[listIdNode[0][0],listIdNode[0][1]]]
+        elif listIdNode[w][1] > medNode[0][1]:
+           medNode = [[listIdNode[w][0], listIdNode[w][1]]]
+        elif (listIdNode[w][1])== (medNode[0][1]):
+           medNode.append([listIdNode[w][0], listIdNode[w][1]])
 
-    Matrx = Nodes_Mean(graph)
-    NodeM = Matrx[n1][n2]
 
-    return NodeM
+    #verifica per caso limite in cui non esiste nessun nodo medio
+    #tempo di esecuzione teorico O(1)
+    if medNode[0][1]==0:
+      medNode = []
+
+
+    return(medNode)
+
+
+
+
 
 if __name__ == "__main__":
 
-    #graph = Graph_Test.generateGraph()
-    graph = Graph_Test.getGraphFix2()
+    graph = Graph_Test.generateGraph()
+    #graph = Graph_Test.getGraphFix2()
 
     D = FloydWarshall_ADJ(graph)
-
-    M = Nodes_Mean(graph)
 
     print ("---- print del grafo ----")
     graph.print()
@@ -82,11 +115,5 @@ if __name__ == "__main__":
     for elm in D:
         print (elm)
 
-    print ("---- Matrice dei Medi dei minimi percorsi ----")
-    for elm in M:
-        print (elm)
-
-    n1,n2 = random.sample(range(0 , graph.numNodes()) , 2)
-    print ("---- Nodo medio tra {} e {} ----".format(n1,n2))
-    N = getNodeMedium(graph,n1,n2)
-    print (N)
+    print("---- Nodo/Nodi medi più frequente ----")
+    print(Nodes_Mean(graph))
