230368a75359aadfbd1a916e653b0b71270e7c46
mikelelandi <34867783+mikelelandi@users.noreply.github.com>
2018-01-16 14:55:36 +0100

    Update graph_test.py

--- graph_test.py
+++ graph_test.py
@@ -4,9 +4,9 @@
 	Author:					Michele Landi / Raoul Landi
 	Modified by:			Michele landi / Raoul Landi
 	Matr Raoul Landi:		0258005
-	Matr Michele Landi:
+	Matr Michele Landi:     0243311
 	Date created:			09/01/2018
-	Date last modified:		09/01/2018
+	Date last modified:		13/01/2018
 	Python Version:			3.6
 	File Versione:			1.0.0
 
@@ -36,24 +36,27 @@ class Graph_Test():
 		# instanziamo oGraph come oggetto di tipo GraphAdjacencyList
 		oGraph = GraphAdjacencyList()
 
-		# si stabilisce il numero dei vertici
+
+		# si stabilisce il numero dei vertici totali N del grafo, compreso tra 5 e 50
 		Ntot = random.randrange(5 , 51)
 
-		# aggiungiamo i vertici al grafo tempo stimato O(Ntot)
+
+		# si aggiungono i vertici al grafo con ID incremntale
+		# tempo di esecuzione Teorico O(N)
 		nodes = []
 		for i in range(0 , Ntot):
 			node = oGraph.addNode(i)
 			nodes.append(node)
-
-		# creiamo una lista A di ID casuali dei nodi del grafo
+		# creazione di una lista IdNodesA composta dai ID dei nodi del grafo scelti casualmente
 		Sample = random.randrange(1 , len(nodes))
 		IdNodesA = random.sample(range(0 , Ntot) , Sample)
-
-		# creiamo una lista B di ID casuali dei nodi del grafo
+		# creazione di una lista IdNodesB composta dai ID dei nodi del grafo scelti casualmente
 		Sample = random.randrange(1 , len(nodes))
 		IdNodesB = random.sample(range(0 , Ntot) , Sample)
 
-		# creazione degli archi
+
+		# creazione degli archi che congiungono in maniera randomica nodi della lista IdNodesA con i nodi della lista IdNodesB
+		# tempo di esecuzione teorico O(N^2)
 		for IdA in IdNodesA:
 			node_src = oGraph.getNode(IdA)
 			for IdB in IdNodesB:
@@ -63,203 +66,29 @@ class Graph_Test():
 					oGraph.insertEdge(node_src.id , node_dst.id , 1)
 					oGraph.insertEdge(node_dst.id , node_src.id , 1)
 
+
+        # simulazione dell'operazione di cancellazione in maniera randomica di un quinto dei nodi e gli eventuali archi ad essi associati
+		# tempo di esecuzione teorico O(1)
+		listNodes=[]
+		for i in range(0, Ntot):
+		 listNodes.append(i)
+		percNode=len(listNodes)//5
+		count=0
+		while count<percNode:
+		 count=count+1
+		 iDeleteNode = random.randrange(0,len(listNodes))
+		 listAdj=oGraph.getAdj(listNodes[iDeleteNode])
+		 for i in range (0,len(listAdj)):
+		   oGraph.deleteEdge(listNodes[iDeleteNode],listAdj[i])
+		 oGraph.deleteNode(listNodes[iDeleteNode])
+		 listNodes.pop(iDeleteNode)
+
 		return oGraph
 
-	def getGraphFix():
-			"""
-			:return: un grafo non orientato con una struttura predifinita con la seguente lista di adiacenza:
-
-					0:[1, 3, 6, 5, 4]
-					1:[6, 0]
-					2:[]
-					3:[6, 0]
-					4:[6, 0]
-					5:[6, 0]
-					6:[1, 3, 0, 5, 4]
-					7:[]
-
-					Il risultato corrisponde a un grafo non orientato con 8 vertici dei quali 2 non sono collegati e con 18 archi
-			"""
-
-			# instanziamo oGraph come oggetto di tipo GraphAdjacencyList
-			oGraph = GraphAdjacencyList()
-
-
-			#creazione degli nodi da 0 a 7
-			nodes = []
-			for i in range(0 , 8):
-				node = oGraph.addNode(i)
-				nodes.append(node)
-
-			# creazione degli arci che partono dal nal nodo 0
-			oGraph.insertEdge(0 ,1, 1)
-			oGraph.insertEdge(0 ,3, 1)
-			oGraph.insertEdge(0 ,6, 1)
-			oGraph.insertEdge(0 ,5, 1)
-			oGraph.insertEdge(0 ,4, 1)
-
-			# creazione degli arci che partono dal nal nodo 1
-			oGraph.insertEdge(1 ,6 , 1)
-			oGraph.insertEdge(1 ,0 , 1)
-
-			# creazione degli arci che partono dal nal nodo 2
-			# vuoto
-
-			# creazione degli arci che partono dal nal nodo 3
-			oGraph.insertEdge(3 ,6 , 1)
-			oGraph.insertEdge(3 ,0 , 1)
-
-			# creazione degli arci che partono dal nal nodo 4
-			oGraph.insertEdge(4 ,6 , 1)
-			oGraph.insertEdge(4 ,0 , 1)
-
-			# creazione degli arci che partono dal nal nodo 5
-			oGraph.insertEdge(5 ,6 , 1)
-			oGraph.insertEdge(5 ,0 , 1)
-
-			# creazione degli arci che partono dal nal nodo 6
-			oGraph.insertEdge(6 ,1 , 1)
-			oGraph.insertEdge(6 ,3 , 1)
-			oGraph.insertEdge(6 ,0 , 1)
-			oGraph.insertEdge(6 ,5 , 1)
-			oGraph.insertEdge(6 ,4 , 1)
-
-			# creazione degli arci che partono dal nal nodo 7
-			# vuoto
-
-			return oGraph
-
-	def getGraphFix2():
-			"""
-			:return: un grafo non orientato con una struttura predifinita con la seguente lista di adiacenza:
-
-					0:[1, 4]
-					1:[0, 2]
-					2:[1]
-					3:[4]
-					4:[0, 3, 5]
-					5:[4, 6]
-					6:[5]
-					7:[]
-
-					Il risultato corrisponde a un grafo non orientato con 7 vertici dei quali 2 non sono collegati e con 18 archi
-			"""
-			# instanziamo oGraph come oggetto di tipo GraphAdjacencyList
-			oGraph = GraphAdjacencyList()
-
-
-			#creazione degli nodi da 0 a 7
-			nodes = []
-			for i in range(0 , 8):
-				node = oGraph.addNode(i)
-				nodes.append(node)
-
-			# creazione degli arci che partono dal nal nodo 0
-			oGraph.insertEdge(0 ,1, 1)
-			oGraph.insertEdge(0 ,4, 1)
-
-			# creazione degli arci che partono dal nal nodo 1
-			oGraph.insertEdge(1 ,0 , 1)
-			oGraph.insertEdge(1 ,2 , 1)
-
-			# creazione degli arci che partono dal nal nodo 2
-			oGraph.insertEdge(2 ,1 , 1)
-
-			# creazione degli arci che partono dal nal nodo 3
-			oGraph.insertEdge(3 ,4 , 1)
-
-			# creazione degli arci che partono dal nal nodo 4
-			oGraph.insertEdge(4 ,0 , 1)
-			oGraph.insertEdge(4 ,3 , 1)
-			oGraph.insertEdge(4, 5 , 1)
-
-			# creazione degli arci che partono dal nal nodo 5
-			oGraph.insertEdge(5 ,4 , 1)
-			oGraph.insertEdge(5 ,6 , 1)
-
-			# creazione degli arci che partono dal nal nodo 6
-			oGraph.insertEdge(6 ,5 , 1)
-
-			# creazione degli arci che partono dal nal nodo 7
-			# vuoto
-
-			return oGraph
-
-	def getGraphFix3():
-			"""
-			:return: un grafo non orientato con una struttura predifinita con la seguente lista di adiacenza:
-
-					0:[1, 3]
-					1:[0, 2]
-					2:[1]
-					3:[0, 4, 5]
-					4:[3]
-					5:[3, 6]
-					6:[5]
-					7:[]
-
-					Il risultato corrisponde a un grafo non orientato con 7 vertici dei quali 2 non sono collegati e con 18 archi
-			"""
-
-			oGraph = GraphAdjacencyList()
-
-
-			#creazione degli nodi da 0 a 7
-
-			nodes = []
-			for i in range(0 , 8):
-				node = oGraph.addNode(i)
-				nodes.append(node)
-
-			# creazione degli arci che partono dal nal nodo 0
-			oGraph.insertEdge(0 ,1, 1)
-			oGraph.insertEdge(0 ,3, 1)
-
-			# creazione degli arci che partono dal nal nodo 1
-			oGraph.insertEdge(1 ,0 , 1)
-			oGraph.insertEdge(1 ,2 , 1)
-
-			# creazione degli arci che partono dal nal nodo 2
-			oGraph.insertEdge(2 ,1 , 1)
-
-			# creazione degli arci che partono dal nal nodo 3
-			oGraph.insertEdge(4 ,3 , 1)
-
-			# creazione degli arci che partono dal nal nodo 4
-			oGraph.insertEdge(3 ,0 , 1)
-			oGraph.insertEdge(3 ,4 , 1)
-			oGraph.insertEdge(3, 5, 1)
-
-			# creazione degli arci che partono dal nal nodo 5
-			oGraph.insertEdge(5 ,3 , 1)
-			oGraph.insertEdge(5 ,6 , 1)
-
-			# creazione degli arci che partono dal nal nodo 6
-			oGraph.insertEdge(6 ,5 , 1)
-
-			# creazione degli arci che partono dal nal nodo 7
-			# vuoto
-
-			return oGraph
 
 if __name__ == "__main__":
 
 	print ("----------  generateGraph  ----------------------")
 	print("--- stampa del grafo ---")
 	Gph = Graph_Test.generateGraph()
-	Gph.print()
-
-	print ("----------  getGraphFix  ----------------------")
-	print("--- stampa del grafo ---")
-	Gph = Graph_Test.getGraphFix()
-	Gph.print()
-
-	print ("----------  getGraphFix2  ----------------------")
-	print("--- stampa del grafo ---")
-	Gph = Graph_Test.getGraphFix2()
-	Gph.print()
-
-	print ("----------  getGraphFix3  ----------------------")
-	print("--- stampa del grafo ---")
-	Gph = Graph_Test.getGraphFix3()
 	Gph.print()
